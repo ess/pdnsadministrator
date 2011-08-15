8a1db1d99f8be9f0449b1ec9edad424d457d9acf
arthmoor@gmail.com <arthmoor@gmail.com@62b29017-d841-84a1-a5f7-30d937a5c774>
2011-08-15 07:51:57 +0000

    Allow any TLD to comply with recent ICANN rulings.

--- global.php
+++ global.php
@@ -575,10 +575,13 @@ if (!defined('PDNSADMIN')) {
 	 */
 	function is_valid_domain($domain)
 	{
-		if( ( preg_match( "/^([a-z0-9]([-a-z0-9]*[a-z0-9])?\\.)+((a[cdefgilmnoqrstuwxz]|aero|arpa)|(b[abdefghijmnorstvwyz]|biz)|(c[acdfghiklmnorsuvxyz]|cat|com|coop)|d[ejkmoz]|(e[ceghrstu]|edu)|f[ijkmor]|(g[abdefghilmnpqrstuwy]|gov)|h[kmnrtu]|(i[delmnoqrst]|info|int)|(j[emop]|jobs)|k[eghimnprwyz]|l[abcikrstuvy]|(m[acdghklmnopqrstuvwxyz]|mil|mobi|museum)|(n[acefgilopruz]|name|net)|(om|org)|(p[aefghklmnrstwy]|pro)|qa|r[eouw]|s[abcdeghijklmnortvyz]|(t[cdfghjklmnoprtvwz]|travel)|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw])$/i", $domain ) ) && ( strlen($domain) <= 64 ) ) {
-			return true;
-		}
-		return false;
+		// This has been gutted to return true at all times now due to recent ICANN rulings on generic TLDs. Oh well.
+		return true;
+
+		// if( ( preg_match( "/^([a-z0-9]([-a-z0-9]*[a-z0-9])?\\.)+((a[cdefgilmnoqrstuwxz]|aero|arpa)|(b[abdefghijmnorstvwyz]|biz)|(c[acdfghiklmnorsuvxyz]|cat|com|coop)|d[ejkmoz]|(e[ceghrstu]|edu)|f[ijkmor]|(g[abdefghilmnpqrstuwy]|gov)|h[kmnrtu]|(i[delmnoqrst]|info|int)|(j[emop]|jobs)|k[eghimnprwyz]|l[abcikrstuvy]|(m[acdghklmnopqrstuvwxyz]|mil|mobi|museum)|(n[acefgilopruz]|name|net)|(om|org)|(p[aefghklmnrstwy]|pro)|qa|r[eouw]|s[abcdeghijklmnortvyz]|(t[cdfghjklmnoprtvwz]|travel)|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw])$/i", $domain ) ) && ( strlen($domain) <= 64 ) ) {
+		//	return true;
+		// }
+		// return false;
 	}
 
 	/**
